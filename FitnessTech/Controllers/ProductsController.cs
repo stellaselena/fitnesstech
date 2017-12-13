37f639a8158cc2c9308248c394669818964be2f7
Stella Jovanovic <stella.jovanovic@avanade.com>
2017-12-13 14:55:48 +0100

    css - full width home page image and fixed css for controllers

---
+++ FitnessTech/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FitnessTech.Data;
+using FitnessTech.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FitnessTech.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ProductsController
+    {
+        private readonly IFitnessRepository _repository;
+        private readonly ILogger _logger;
+
+        public ProductsController(IFitnessRepository repository, ILogger logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
